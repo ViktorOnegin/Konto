3456b0819e9eb3b5c357ba9f79473260dba0164a
ViktorOnegin <33310165+ViktorOnegin@users.noreply.github.com>
2017-11-16 09:51:08 +0200

    Kontrolltöö 2

---
+++ 3).py
@@ -0,0 +1,14 @@
+List1 = [11, 15, 6, 13, 13, 25, 32, 11, 20, 5, 31, 16, 32, 29, 11, 13, 3, 29, 28, 24]
+List2 = [5, 19, 16, 4, 12, 7, 2, 28, 34, 29, 29, 36, 6, 8, 24, 18, 31, 7, 1, 7]
+
+print("Esimese listi suurim number")
+print(max(List1))
+print("Esimese listi väiksem number")
+print(min(List1))
+
+print("teise listi suurim number")
+print(max(List2))
+print("teise listi väiksem number")
+print(min(List2))
+
+
